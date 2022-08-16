a19f509ba2292e2fa7e6826436acc0f19c050539
OmarHAmer <omar.hesham.alex2016@gmail.com>
2022-08-16 23:54:38 +0200

    ADD filter on job

--- KOPPEE/views.py
+++ KOPPEE/views.py
@@ -3,17 +3,22 @@ from .models import Job
 from .forms import ApplyForms
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
+from .filters import JobFilter
 # Create your views here.
 
 def job_list(request):
     job_list = Job.objects.all()
 
-    paginator = Paginator(job_list, 1)
+    formjobfilter = JobFilter(request.GET, queryset=job_list)
+    jobfilter = formjobfilter.qs
+
+    paginator = Paginator(jobfilter, 1)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
     context = {
-        'jobs' : page_obj
+        'jobs' : page_obj,
+        'filter' : formjobfilter
     }
     return render(request,'job/job_list.html',context)
     
