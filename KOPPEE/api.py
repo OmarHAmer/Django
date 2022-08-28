790cb253bb1edd6fec6c3301d5f85a01dd75291a
OmarHAmer <omar.hesham.alex2016@gmail.com>
2022-08-28 16:03:55 +0200

    Apply Serializer on project

--- KOPPEE/api.py
+++ KOPPEE/api.py
@@ -2,12 +2,34 @@ from .models import Job
 from .serializers import JobSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import generics
 
-@api_view(['GET', 'POST'])
-def jobapi(request):
+# @api_view(['GET', 'POST'])
+# def jobapi(request):
 
-    job = Job.objects.all()
+#     job = Job.objects.all()
 
-    data = JobSerializer (job,many=True).data
+#     data = JobSerializer (job,many=True).data
 
-    return Response({'data':data})
+#     return Response({'data':data})
+
+# @api_view(['GET'])
+# def job_details(request,id):
+
+#     job = Job.objects.get(id =id)
+
+#     data = JobSerializer(job).data
+
+#     return Response({'data':data})
+
+
+class JobApi(generics.ListAPIView):
+    queryset = Job.objects.all()
+    serializer_class = JobSerializer
+
+
+class JobDetails(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Job.objects.all()
+    serializer_class = JobSerializer
+    lookup_field = 'id'
+    
--- KOPPEE/urls.py
+++ KOPPEE/urls.py
@@ -4,6 +4,11 @@ from . import views ,api
 app_name = 'KOPPEE'
 urlpatterns = [
     path('', views.job_list,name = 'home'),
-    path('api/list', api.jobapi,name = 'jobapi'),
     path('<str:slug>',views.job_detail,name='job_detail'),
+
+    # path('api/list', api.jobapi,name = 'jobapi'),
+    # path('api/list/<int:id>', api.job_details,name = 'job_details'),
+
+    path('api/list', api.JobApi.as_view(),name = 'JobApi'),
+    path('api/list/<int:id>', api.JobDetails.as_view(),name = 'JobDetails'),
 ]
